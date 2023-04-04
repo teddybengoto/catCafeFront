2808bbe990f5c46ad9fc576920e5952b30a331ab
teddybengoto <teddybengoto@gmail.com>
2023-04-04 15:30:33 +0200

    Add compte update form

--- src/app/component/compte/compte.component.ts
+++ src/app/component/compte/compte.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Compte } from 'src/app/model/compte';
 import { CompteService } from 'src/app/sevice.api/compte.service';
@@ -11,16 +12,31 @@ import { CompteService } from 'src/app/sevice.api/compte.service';
 export class CompteComponent {
 
   myCompte:Compte;
+  updateCompte!: FormGroup;
 
 
-  constructor(private compteService: CompteService, private router:Router ){
+  constructor(private formBuilder: FormBuilder ,private compteService: CompteService, private router:Router ){
+
 
     
     if (!compteService.auth?.id) {
       this.router.navigate(['/connexion']);
     }
+
+
+    this.updateCompte = this.formBuilder.group({
+      nom: this.formBuilder.control('', Validators.required),
+      prenom: this.formBuilder.control('', [Validators.required, Validators.minLength(8)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.email]),
+      telephone: this.formBuilder.control('', Validators.required)
+
+    });
+
+
+
     //this.getClient()  ;
     this.findClientDetail();
+
 
   }
 
@@ -28,6 +44,13 @@ export class CompteComponent {
 
   getClient(){
     this.compteService.findClientById(this.compteService.auth.id);
+  }
+
+  update(){
+
+    console.log("update");
+    
+
   }
 
   findClientDetail():void{
