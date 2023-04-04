23284565614892938465094b6246659a92850f06
Jordan Rolland <jordan.rolland78@gmail.com>
2023-04-04 19:33:58 +0200

    Animation dynamique

--- src/app/component/reservation/reservation.component.ts
+++ src/app/component/reservation/reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { Espace } from 'src/app/model/espace';
 import { Reservation } from 'src/app/model/reservation';
@@ -12,6 +12,7 @@ import { ReservationService } from 'src/app/sevice.api/reservation.service';
 })
 export class ReservationComponent {
   reservationForm: Reservation = new Reservation();
+  plan: string ="../../../assets/img/Plan_Standard.jpg";
 
   keys= Object.keys;
   espaces = Espace;
@@ -28,6 +29,24 @@ export class ReservationComponent {
     alert("Votre réservation a bien été pris en compte. Merci !");
     this.router.navigate(['/']);
   }
+
+  @HostListener('change', ['$event.target'])
+  modifPlan() {
+    console.log("test : ", this.reservationForm.espace);
+    console.log("test2", this.reservationForm.espace.toString());
+    switch (this.reservationForm.espace.toString())
+    {
+      case null:{this.plan = "../../../assets/img/Plan_Standard.jpg";  console.log("general"); break;}
+      case "Chill":{this.plan = "../../../assets/img/Plan_Chill.jpg"; console.log("general"); break;}
+      case "Coworking":{this.plan = "../../../assets/img/Plan_Cowork.jpg"; console.log("general"); break;}
+      case "Jeu":{this.plan = "../../../assets/img/Plan_Jeu.jpg"; console.log("general"); break;}
+      case "SalonDeThe":{this.plan = "../../../assets/img/Plan_SalonThe.jpg"; console.log("general"); break;}
+    }
+
+   
+    
+  }
+
 
 
 
