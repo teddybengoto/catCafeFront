e986ae25bf7f56cc0cb94dd45dd81f7f45a93fdc
ThomasAbrial <thomas.abrial@laposte.net>
2023-04-06 13:36:28 +0200

    formulaire chat admin ca marche enfin

--- src/app/model/chat.ts
+++ src/app/model/chat.ts
@@ -12,8 +12,7 @@ export class Chat {
     commentaire : string;
     image : string;
     naissance : string;
-    clientId : number;
-    client : any;
+    client ?: any;
 
     constructor(id?: number,
         nom ?: string,
@@ -28,7 +27,6 @@ export class Chat {
         commentaire ?: string,
         image ?: string,
         naissance ?: string,
-        clientId ?: number,
         client ?: any){
             this.nom=nom;
             this.sexe=sexe;
@@ -42,7 +40,52 @@ export class Chat {
             this.commentaire=commentaire;
             this.image=image;
             this.naissance=naissance;
-            this.clientId=clientId;
             this.client=client;
+        }
+}
+
+export class ChatSend {
+    id: number;
+    nom : string;
+    sexe : string;
+    race : string;
+    adoptable : boolean;
+    permanent : boolean;
+    sterile : boolean;
+    idPuce : string;
+    idTatouage : string;
+    pbSante : string;
+    commentaire : string;
+    image : string;
+    naissance : string;
+    clientId ?: number;
+
+    constructor(id?: number,
+        nom ?: string,
+        sexe ?: string,
+        race ?: string,
+        adoptable ?: boolean,
+        permanent ?: boolean,
+        sterile ?: boolean,
+        idPuce ?: string,
+        idTatouage ?: string,
+        pbSante ?: string,
+        commentaire ?: string,
+        image ?: string,
+        naissance ?: string,
+        clientId ?: number,){
+            this.nom=nom;
+            this.sexe=sexe;
+            this.race=race;
+            this.adoptable=adoptable;
+            this.permanent=permanent;
+            this.sterile=sterile;
+            this.idPuce=idPuce;
+            this.idTatouage=idTatouage;
+            this.pbSante=pbSante;
+            this.commentaire=commentaire;
+            this.image=image;
+            this.naissance=naissance;
+            this.clientId=clientId;
         }
 }
